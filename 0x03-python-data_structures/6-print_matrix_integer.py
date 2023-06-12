421cb3cb5be34aa155006e0d63ebab6e590deee1
Abdullahi <abdush504@gmail.com>
2023-06-12 12:54:39 -0700

    changed syntax

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end="" if row.index(element) == len(row) - 1 else " ")
+    for line in matrix:
+        for col in range(len(line)):
+            print(
+                "{:d}".format(line[col]),
+                end="" if col == len(line) - 1 else " ")
         print("")
