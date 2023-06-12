40455c84fed7ff6de1a527f4f289e6b0c8310105
Abdullahi <abdush504@gmail.com>
2023-06-12 01:49:20 -0700

    bug fix

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    elif len(my_list) >= idx:
-        return my_list
-    my_list[idx] = element
+    if (0 <= idx < len(my_list)):
+        my_list[idx] = element
     return my_list
