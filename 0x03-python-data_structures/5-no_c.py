5cb3039c76b6a7cade4b504cd1e9e0aeb9d1d74d
Abdullahi <abdush504@gmail.com>
2023-06-12 12:18:50 -0700

    changed syntax

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def no_c(my_string):
-    translation_table = str.maketrans("", "", "cC")
-    translated_text = my_string.translate(translation_table)
-    return translated_text
+    return my_string.translate({ord(c): None for c in "cC"})
