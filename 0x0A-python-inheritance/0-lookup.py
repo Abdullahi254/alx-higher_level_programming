9a0e610442ce2cb5387a53fd561540c24bf9a055
Abdullahi <abdush504@gmail.com>
2023-07-10 04:32:40 -0700

    added a fullstop at the end of the comment

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,5 @@
 """Defines an object attribute lookup function."""
 
 def lookup(obj):
-    """iterates through the attributes and return a list of attributes"""
+    """iterates through the attributes and return a list of attributes."""
     return (dir(obj))
