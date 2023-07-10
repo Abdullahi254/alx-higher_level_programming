695aedd43dd02f00f8f8ae7a551d851c6ada6263
Abdullahi <abdush504@gmail.com>
2023-07-10 05:25:35 -0700

    added a whitespace

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,6 +3,7 @@
 
 """Defines a class-checking function."""
 
+
 def is_same_class(obj, a_class):
     """ returns True if the object is exactly an instance of the specified class ; otherwise False.
     Args:
