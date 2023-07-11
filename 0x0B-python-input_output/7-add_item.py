a67e5193349960af1a208386d9e69d78648a899c
Abdullahi <abdush504@gmail.com>
2023-07-11 11:44:43 -0700

    added an if statement

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,17 +4,20 @@ import modules and sys function
 """
 from sys import argv
 import json
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file =\
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
         __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    j_list = load_from_json_file('add_item.json')
-except:
-    j_list = []
+    try:
+        j_list = load_from_json_file('add_item.json')
+    except:
+        j_list = []
 
-for i in range(1, len(argv)):
-    j_list.append(argv[i])
+    for i in range(1, len(argv)):
+        j_list.append(argv[i])
 
-save_to_json_file(j_list, 'add_item.json')
+    save_to_json_file(j_list, 'add_item.json')
 
