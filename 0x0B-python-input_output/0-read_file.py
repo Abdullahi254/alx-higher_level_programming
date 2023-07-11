12d4ef06642b00d31cdb5cebca900854903937aa
Abdullahi <abdush504@gmail.com>
2023-07-11 05:27:43 -0700

    add a function that reads and prints out file content

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 0-read_file.py
+
+"""Defines read_file()"""
+
+def read_file(filename=""):
+    """prints out file content"""
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
