35c08297ff65947ec0262f0cc1d645f42c741cf4
Abdullahi <abdush504@gmail.com>
2023-08-17 03:48:34 -0700

    added comments

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Show priviledges for `user_0d_1` and `user_0d_2`.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- show privilege for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- grant all privilege
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+-- reload grant table
 FLUSH PRIVILEGES;
