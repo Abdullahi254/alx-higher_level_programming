f676d2d4b4525bb9ff7b65744441e35f45b02472
Abdullahi <abdush504@gmail.com>
2023-08-17 14:00:43 -0700

    added outer key word

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- lists all genres the show dexter is listed under
 SELECT tv_shows.title, tv_genres.name
        FROM tv_shows
-       LEFT JOIN tv_show_genres
+       LEFT OUTER JOIN tv_show_genres
        ON tv_shows.id = tv_show_genres.show_id
-       	  LEFT JOIN tv_genres
+       	  LEFT OUTER JOIN tv_genres
 	  ON tv_genres.id = tv_show_genres.genre_id
        ORDER BY tv_shows.title;
