77e2254b9d7758a7d93f3ef6001e2ce97222ff64
Abdullahi <abdush504@gmail.com>
2023-08-17 04:37:33 -0700

    table states created in hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creating databse hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- creating table states in hbtn_0d_usa
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+name VARCHAR(256), PRIMARY KEY (id));
