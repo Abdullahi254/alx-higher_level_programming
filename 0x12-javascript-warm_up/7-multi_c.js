5bb64c36bbcb26e16c66d405633fd650028df70d
Abdullahi254 <abdush504@gmail.com>
2023-09-11 11:57:38 +0300

    added condition before printing

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,11 @@ import { argv } from 'node:process';
 
 let x = Number(argv[0]);
 
-while (x > 0) {
-    console.log("C is fun");
-    x -= 1;
+if (typeof (x) == "number") {
+    while (x > 0) {
+        console.log("C is fun");
+        x -= 1;
+    }
+} else {
+    console.log("Missing number of occurrences");
 }
