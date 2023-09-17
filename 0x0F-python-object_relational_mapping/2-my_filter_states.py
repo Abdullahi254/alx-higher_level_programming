d0b6f785b17d5556e394b52fc2ce4f9bcf55ad8d
Abdullahi <abdush504@gmail.com>
2023-09-17 04:17:33 -0700

    changed variable name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-2-my_filter_states.py - displays all values in the states table of hbtn_0e_0_usa with name
-that has the name same as provided by user
+2-my_filter_states - displays all values whose name matches the passed argument
+in the states table of hbtn_0e_0_usa
 """
 
 if __name__ == "__main__":
@@ -14,20 +14,21 @@ if __name__ == "__main__":
     user = av[1]
     passwd = av[2]
     db = av[3]
-    searched_name = av[4]
+    search = av[4]
 
     host = "localhost"
-    port = 3306
+    port = 5432
 
     conn = MySQLdb.connect(host=host, port=port,
                            user=user, passwd=passwd, db=db)
 
     cur = conn.cursor()
+
     cur.execute(
         """SELECT * FROM states
         WHERE name = _utf8mb4 '{:s}' COLLATE utf8mb4_0900_as_cs
-        ORDER BY id ASC""".format(searched_name)
-        )
+        ORDER BY id ASC""".format(search))
+
     rows = cur.fetchall()
 
     for row in rows:
