a6bbeafd3ea0c67466183574edb96323660f4d5e
Abdullahi <abdush504@gmail.com>
2023-09-17 13:45:07 -0700

    created base state model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+model_state.py - defines a class `State` and an instance
+`Base` of declarative_base from SQLAlchemy.
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from model_base import Base
+
+
+class State(Base):
+    """State class inherits from Base"""
+
+    __tablename__ = 'states'
+    id = Column('id', Integer, primary_key=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
+
