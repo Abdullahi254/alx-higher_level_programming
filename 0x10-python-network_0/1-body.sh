967871fd55e89be314a2eecf6dec6d3255ae2ba2
Abdullahi <abdush504@gmail.com>
2023-09-28 13:22:03 -0700

    curl -s

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-response=$(curl -sI "$1" | grep "200" | cut -d " " -f2)
-if [[ "$response" =~ ^[0-9]+$ ]]; then
-	if [ "$response" -eq 200 ]; then
-  		curl -s "$1"
-	fi
-fi
+# This script takes in a URL, sends GET request and displays body of response
+curl -s "$1"
