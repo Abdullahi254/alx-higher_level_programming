05e8580d7702c59ff163074f1823d415b448270d
Abdullahi <abdush504@gmail.com>
2023-09-28 10:40:24 -0700

    bug fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep -q "200" && curl "$1"
+# This script takes in a URL, sends GET request and displays body of response
+curl -sL "$1
