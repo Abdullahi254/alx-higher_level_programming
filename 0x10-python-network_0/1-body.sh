803accb7ce794922cae47ed3893f925a89c07f56
Abdullahi <abdush504@gmail.com>
2023-09-28 11:09:22 -0700

    will checker pass this?

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# This script takes in a URL, sends GET request and displays body of response
-curl -sL "$1
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+
+response=$(curl -sI "$1" | grep "200" | cut -d " " -f2)
+if [[ "$response" =~ ^[0-9]+$ ]]; then
+	if [ "$response" -eq 200 ]; then
+  		curl -s "$1"
+	fi
+fi
