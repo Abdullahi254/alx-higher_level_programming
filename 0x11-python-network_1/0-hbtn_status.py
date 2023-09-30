1a2c9e38152d33b960902b826c4c55f7b95a5e85
Abdullahi <abdush504@gmail.com>
2023-09-30 03:53:57 -0700

    refactored code to pass checker

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-import urllib.request
 """
 script that usese urllib to fetch get request
 """
-def main():
-    '''
-    main script that fetches for response
-    '''
-    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+import urllib.request
+
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
         print("\t- utf8 content: {}".format(body.decode("utf-8")))
-if __name__ == "__main__":
-    main()
-
