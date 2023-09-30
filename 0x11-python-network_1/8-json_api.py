14a8785b70ae82d6f7b931c135b42adbabc26e3c
Abdullahi254 <abdush504@gmail.com>
2023-09-30 14:42:36 -0700

    getting JSON response

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("No result")
+    else:
+        try:
+            url = "http://0.0.0.0:5000/search_user"
+            q = sys.argv[1]
+            r = requests.post(url, data={"q": q if q is not None else ""})
+            if not r.json():
+                print("No result")
+            else:
+                print("[{}] {}".format(r.json().get("id"), r.json().get("name")))
+        except requests.exceptions.JSONDecodeError:
+            print("Not a valid JSON")
+
