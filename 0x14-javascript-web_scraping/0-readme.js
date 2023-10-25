d07642d5b9b65d47b758f16e9c6e60e9cb368a0f
root <abdush504@gmail.com>
2023-10-25 04:52:50 -0700

    using console error to print error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const request = require("request");
 const fs = require("fs");
 
 const filePath = process.argv[2];
 
 fs.readFile(filePath, "utf8", (err, data)=>{
 	if (err){
-		console.log(err);
+		console.error(err);
 	}else {
 		console.log(data);
 	}
