b9a5dcaf65c19bb60e82791534bb7f893b9cdfe7
root <abdush504@gmail.com>
2023-10-25 09:00:25 -0700

    semistandardizing code

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -6,12 +6,12 @@ const request = require('request');
 const url = process.argv[2];
 const file = process.argv[3];
 
-request(url,(error, response, body)=>{
- if (error){
+request(url,(error, response, body) => {
+ if (error) {
   console.log(error);
  } else {
-  fs.writeFile(file, body, "utf-8", (err)=>{
+  fs.writeFile(file, body, "utf-8", (err) => {
    if (err) console.log(err);
-  })
- }
-})
+  });
+ };
+});
